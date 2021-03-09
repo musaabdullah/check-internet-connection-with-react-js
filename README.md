e40e1a92ef5aa880e34d3f0269df47922b63b56e
musaabdullah <musaabdalwahed@gmail.com>
2021-03-09 22:26:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Getting Started with Create React App
+
+![Screenshot_2021-03-08 React App(3)](https://user-images.githubusercontent.com/60151264/110342810-81d7f280-8034-11eb-8872-e5d202fff07c.png)
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+### Deployment
+
+### `npm run build` fails to minify
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+function App() {
+  
+  const [connection, setConnection] = useState(false);
+  const [visiable, setVisiable] = useState(true);
+  useEffect(() => {
+     const checkInternet = async () => {
+       try {
+
+         const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+         console.log(res);
+         if(res.status === 200) {
+           setConnection(true);
+          }else {
+            setConnection(false);
+          }
+        } catch(error) {
+          console.log(error.message);
+        }
+     }
+     checkInternet();
+  }, [])
+  
+ 
+
+
+  return (
+    <div className="app">
+        <div className={` ${visiable ? "wrapper__show": "wrapper__hide"} wrapper`}>
+         <div className="wrapper__content">
+             <i className={`${connection ? "content__icon--online":"content__icon--offline"} fa fa-wifi`}></i>
+             <div className="wrapper__detail">
+                 <div className="detail__header">
+                    <span>{connection ? "Online" : "offline"}</span>
+                 </div>
+                 <div className="detail__body">
+                   You are {connection ? "online" : "offline"} now.
+                 </div>
+             </div>
+             <i onClick={() => setVisiable(false)}  className="wrapper__close	fa fa-close"></i>
+         </div>
+      </div> 
+    </div>
+  ); 
+}
+
+export default App;
